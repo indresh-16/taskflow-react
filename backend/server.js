d040fc625782f5e6d495fbe348b120ff3f13a92a
indresh-16 <indreshindresh603@gmail.com>
2026-09-01 12:36:58 +0530

    Prepare TaskFlow for deployment

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,6 @@ app.use(express.json());
 app.use("/", authRoutes);
 app.use("/", taskRoutes);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(5000, () => {
+  console.log("server running on http://localhost:5000");
 });
