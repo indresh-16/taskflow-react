59fe2e7861e45b6280465989a38f9a3726726bc7
indresh-16 <indreshindresh603@gmail.com>
2026-09-02 13:50:52 +0530

    Add backend health check

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,20 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
+app.use(cors());
 
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 app.get("/", (req, res) => {
   res.send("TaskFlow Backend is Running 🚀");
 });
-app.use(cors());
+app.get("/health", (req, res) => {
+  res.json({
+    status: "OK",
+    database: "connected"
+  });
+});
+
 app.use(express.json());
 app.use("/", authRoutes);
 app.use("/", taskRoutes);
